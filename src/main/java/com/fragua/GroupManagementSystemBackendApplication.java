c63c8dc2770723467e7765e9eb8a5c83a84133d6
Artur Paszkiewicz <paszkiewicz.artur@gmail.com>
2018-04-24 15:23:49 +0200

    initial commit

---
+++ src/main/java/com/fragua/GroupManagementSystemBackendApplication.java
@@ -0,0 +1,12 @@
+package com.fragua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupManagementSystemBackendApplication.class, args);
+	}
+}
